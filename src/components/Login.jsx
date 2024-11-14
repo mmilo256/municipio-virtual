eb903961d54097920957b258d00937d8ae35024e
Emilio Soto Andrade <emiliosotoandrade256@gmail.com>
2024-11-14 17:25:23 -0300

    add: responsive en pantalla de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,27 +10,30 @@ const Login = () => {
         navigate("/inicio")
     }
     return (
-        <div className="grid grid-cols-7 min-h-svh bg-slate-50">
-            <div className="relative col-span-4 bg-[url('/chonchi-aereo.jpg')]">
+        <div className="grid lg:grid-cols-7 min-h-svh bg-slate-50">
+            <div className="relative h-72 lg:h-auto col-span-4 bg-[url('/chonchi-aereo.jpg')]">
                 <div className="absolute inset-0 bg-primary bg-opacity-85 flex flex-col justify-center text-center items-center text-white">
-                    <h2 className="text-4xl opacity-50">Ilustre Municipalidad de Chonchi</h2>
-                    <h1 className="text-7xl font-bold">Municipio <span className="text-secondary">Virtual</span></h1>
+                    <h2 className="text-xl md:text-4xl opacity-50">Ilustre Municipalidad de Chonchi</h2>
+                    <h1 className="text-4xl md:text-7xl font-bold">Municipio <span className="text-secondary">Virtual</span></h1>
+                    <div className="mt-10 w-full px-8 md:hidden">
+                        <BotonClaveUnica className="w-full md:w-auto" onClick={login} />
+                    </div>
                 </div>
             </div>
-            <div className="p-16 col-span-3">
-                <h2 className="text-primary font-bold text-4xl mb-4">Municipio Virtual de Chonchi</h2>
+            <div className="p-8 lg:p-16 col-span-3">
+                <h2 className="text-primary text-center lg:text-left font-bold text-4xl mb-4">Municipio Virtual de Chonchi</h2>
                 <div className="text-slate-600 text-xl">
-                    <p className="leading-8">A través de este portal, podrá acceder a una amplia variedad de trámites y servicios ofrecidos por la <strong>Municipalidad de Chonchi.</strong></p>
-                    <p className="my-5">¿Cómo funciona?</p>
-                    <ol className="list-disc list-inside leading-8">
-                        <li>Ingrese al portal utilizando su <strong>ClaveÚnica.</strong></li>
-                        <li>Seleccione el servicio o trámite que desea realizar.</li>
-                        <li>Complete los campos solicitados con la información requerida.</li>
-                        <li>Recibirá notificaciones sobre el estado y resultado de su solicitud a través de correo electrónico u otros medios de contacto disponibles.</li>
+                    <p className="leading-6 text-justify">A través de este portal, podrá acceder a una amplia variedad de trámites y servicios ofrecidos por la <strong>Municipalidad de Chonchi.</strong></p>
+                    <p className="my-5 text-center lg:text-left">¿Cómo funciona?</p>
+                    <ol className="list-disc list-inside">
+                        <li className="mb-2">Ingrese al portal utilizando su <strong>ClaveÚnica.</strong></li>
+                        <li className="mb-2">Seleccione el servicio o trámite que desea realizar.</li>
+                        <li className="mb-2">Complete los campos solicitados con la información requerida.</li>
+                        <li className="mb-2">Recibirá notificaciones sobre el estado y resultado de su solicitud a través de correo electrónico u otros medios de contacto disponibles.</li>
                     </ol>
                 </div>
-                <div className="my-5">
-                    <BotonClaveUnica onClick={login} />
+                <div className="hidden md:block my-5">
+                    <BotonClaveUnica className="w-full md:w-auto lg:w-full ml-auto" onClick={login} />
                 </div>
             </div>
         </div>
--- src/components/ui/BotonClaveUnica.jsx
+++ src/components/ui/BotonClaveUnica.jsx
@@ -1,9 +1,9 @@
 import claveUnica from '../../assets/claveunica.svg'
 
-const BotonClaveUnica = ({ onClick }) => {
+const BotonClaveUnica = ({ onClick, className }) => {
 
     return (
-        <button onClick={onClick} className='w-full transition-colors flex gap-[4px] h-[48px] items-center justify-center bg-[#0F69C4] hover:bg-[#07305A] px-[14px] py-[8px]'>
+        <button onClick={onClick} className={`transition-colors flex gap-[4px] h-[48px] items-center justify-center bg-[#0F69C4] hover:bg-[#07305A] px-[14px] py-[8px] ${className}`}>
             <img width={24} src={claveUnica} alt="" />
             <span className='text-base font-bold text-white'>Iniciar sesión</span>
         </button>
